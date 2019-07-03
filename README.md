12b8e2e3e1a16ca6e1f64e0ab32f5f70bae5bb4b
Aaron Votre <avotre422@gmail.com>
2019-07-02 22:41:22 -0400

    Organize the first script
    add comments, split fxns if possible

---
+++ README.md
@@ -0,0 +1,5 @@
+# Adobe Illustrator Helpers 🎉
+
+These scripts should help copy and scale men's and women's jerseys.
+
+Instructions will go here
--- maskSetup.js
+++ maskSetup.js
@@ -5,34 +5,18 @@ if (app.documents.length > 0) {
   // check for breakmark template
   if (bkDoc.name == "breakmark-template.ai") {
     var bkArtLayer = bkDoc.layers[bkDoc.layers.length - 1];
-    var bkMensMasks = filter(bkArtLayer.groupItems, onlyMensMask);
-    var bkWomensMasks = filter(bkArtLayer.pathItems, onlyWomensMask);
+    var bkMensMasks = filter(bkArtLayer.groupItems, isMaleMask);
+    var bkWomensMasks = filter(bkArtLayer.pathItems, isFemaleMask);
 
     for (var i = 0; i < bkMensMasks.length; i++) {
       var bkWomensArt = findFemaleCounterpart(bkWomensMasks, bkMensMasks[i].name);
+
       if (bkWomensArt != undefined && bkWomensArt != null) {
-        // Now do the thing
         var workingMask = bkMensMasks[i].duplicate(bkWomensArt, ElementPlacement.PLACEAFTER);
-        var workingMaskClip = workingMask.pathItems[0];
-        var scale = bkWomensArt.height / workingMaskClip.height * 100;
-
-        workingMask.resize(scale, scale);
-
-        var center = bkWomensArt.left + bkWomensArt.width / 2;
-        var offsetLeft = workingMaskClip.left + workingMaskClip.width / 2 - workingMask.left;
-        var left = center - offsetLeft;
-        var offsetTop = workingMaskClip.top - workingMask.top;
-        var top = bkWomensArt.top - offsetTop;
-
-        workingMask.position = [left, top];
-        workingMask.name = bkWomensArt.name;
-        bkWomensArt.move(workingMask, ElementPlacement.PLACEATBEGINNING);
-        workingMask.clipped = false;
-        workingMaskClip.remove();
-        bkWomensArt.clipping = true;
-        workingMask.clipped = true;
+        createScaledFemaleMask(workingMask, bkWomensArt);
       }
     }
+
     newFile = new File(bkDoc.path + "/breakmark-all-art.ai");
     bkDoc.saveAs(newFile);
   } else {
@@ -40,6 +24,33 @@ if (app.documents.length > 0) {
   }
 }
 
+// Take the new mask, and use the female mask to move and scale it
+// Warning: This has side effects (AKA if modifies the images), this is the meat.
+// (Mask, Mask) :: NoReturn
+function createScaledFemaleMask(workingMask, femaleArt) {
+  var workingMaskClip = workingMask.pathItems[0];
+  var scale = femaleArt.height / workingMaskClip.height * 100;
+
+  workingMask.resize(scale, scale);
+
+  var center = femaleArt.left + femaleArt.width / 2;
+  var offsetLeft = workingMaskClip.left + workingMaskClip.width / 2 - workingMask.left;
+  var left = center - offsetLeft;
+  var offsetTop = workingMaskClip.top - workingMask.top;
+  var top = femaleArt.top - offsetTop;
+
+  workingMask.position = [left, top];
+  workingMask.name = femaleArt.name;
+  femaleArt.move(workingMask, ElementPlacement.PLACEATBEGINNING);
+  workingMask.clipped = false;
+  workingMaskClip.remove();
+  femaleArt.clipping = true;
+  workingMask.clipped = true;
+}
+
+
+// Filter Female masks searching for a mask matching the mens name
+// (List Mask, String) :: Mask | nil
 function findFemaleCounterpart(womensMasks, mensMaskName) {
   var nameToMatch = "wo" + mensMaskName;
   var counterPart = filter(womensMasks, function (mask) { return mask.name === nameToMatch });
@@ -47,14 +58,19 @@ function findFemaleCounterpart(womensMasks, mensMaskName) {
   return counterPart[0];
 }
 
-function onlyMensMask(mask) {
+
+// Used to filter, returns true only for men's masks
+// ({name: String}) :: Boolean
+function isMaleMask(mask) {
   var firstFour = mask.name.substr(0, 4);
   var lastFour = mask.name.substr(mask.name.length - 4, 4);
 
   return firstFour === "mens" && lastFour === "mask";
 }
 
-function onlyWomensMask(mask) {
+// Used to filter, returns true only for women's masks
+// ({name: String}) :: Boolean
+function isFemaleMask(mask) {
   var firstSix = mask.name.substr(0, 6);
   var lastFour = mask.name.substr(mask.name.length - 4, 4);
 
@@ -62,6 +78,7 @@ function onlyWomensMask(mask) {
 }
 
 // Returns new list
+// (List a, fxn a -> Boolean) :: List a
 function filter(list, fxn) {
   var newList = [];
   for (var i = 0; i < list.length; i++) {
